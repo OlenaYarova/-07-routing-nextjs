3104d6fb752155c860a5d8291630dcd8c3c0a222
Olena <marineelen91@gmail.com>
2026-01-17 16:28:18 +0200

    fix notes

--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -1,6 +1,6 @@
 import {fetchNotes } from '@/lib/api'
 import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
-import NotesClient from '../../Notes.client';
+import NotesClient from './Notes.client';
 
 
 
